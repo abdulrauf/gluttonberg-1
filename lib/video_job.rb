64b4169cf162f15e085830b4b6b7b607cc498b42
Yuri Tomanek <yuri@tomanek.com.au>
2012-02-14 14:38:00 +1030

    Added qtfaststart
    Signed-off-by: Yuri Tomanek <yuri@tomanek.com.au>

--- lib/video_job.rb
+++ lib/video_job.rb
@@ -3,14 +3,16 @@ class VideoJob < Struct.new(:video)
   def perform
     asset = Gluttonberg::Asset.find(video)
     commands = []
+    paths = []
     if Gluttonberg::Setting.get_setting("video_assets") == "Enable"
       ffmpeg_gb_setting = Gluttonberg::VideoSetting.all
       unless ffmpeg_gb_setting.blank?
-        ffmpeg_gb_setting.each do |s|
-          commands << "#{s.command} #{save_asset_to(asset)}/#{asset.filename_without_extension}_#{s.file_postfix}"
+        ffmpeg_gb_setting.each_with_index do |s, i|
+          paths << "#{save_asset_to(asset)}/#{asset.filename_without_extension}_#{s.file_postfix}"
+          commands << "#{s.command} #{paths[i]}"
         end
         options = {:file => asset.absolute_file_path}
-        commands.each do |command|
+        commands.each_with_index do |command, i|
           begin
             puts "Starting to encode #{command}"
             puts "path #{asset.absolute_file_path}"
@@ -19,11 +21,13 @@ class VideoJob < Struct.new(:video)
             #asset.duration = transcoder.processed.duration
             asset.processed = true
             asset.save
+            system("qtfaststart #{paths[i]}")
           rescue => e
             asset.processed = true
             #asset.error = true
             asset.save
             puts "Unable to transcode file: #{e.class} - #{e.message}"
+            system("qtfaststart #{paths[i]}")
           end
         end
         if !Gluttonberg::Setting.get_setting("s3_key_id").blank? && !Gluttonberg::Setting.get_setting("s3_access_key").blank? && !Gluttonberg::Setting.get_setting("s3_server_url").blank? && !Gluttonberg::Setting.get_setting("s3_bucket").blank?
