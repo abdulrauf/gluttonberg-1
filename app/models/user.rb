73c25a11138346ad5191f9c1fd998044523fa879
Abdul <rauf_eng@hotmail.com>
2012-04-04 15:19:24 +0930

    User : Mass assignment fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
+  attr_accessible :first_name , :last_name , :email , :password , :password_confirmation , :bio , :image_id
   
   set_table_name "gb_users"
   belongs_to :images , :foreign_key => "image_id" , :class_name => "Gluttonberg::Asset"
   
   validates_presence_of :first_name , :email , :role
+  validates_format_of :password, :with => /^(?=.*\d)(?=.*[a-zA-Z])(?!.*[^\da-zA-Z]).{6,}$/ , :if => :require_password?, :message => "must be a minimum of 6 characters in length, contain at least 1 letter and at least 1 number"
+  
   
   clean_html [:bio]
   
@@ -32,6 +35,17 @@ class User < ActiveRecord::Base
   
   def self.user_roles
     @roles ||= (["super_admin" , "admin" , "contributor"] << (Rails.configuration.user_roles) ).flatten
+  end
+  
+  def user_valid_roles(user)
+    if user.id == self.id
+      []
+    else  
+      roles = (["super_admin" , "admin" , "contributor"] << (Rails.configuration.user_roles) ).flatten
+      roles.delete("super_admin") unless self.super_admin? 
+      roles.delete("contributor") if !self.super_admin? && !self.admin?
+      roles 
+    end  
   end
   
   def have_backend_access?
